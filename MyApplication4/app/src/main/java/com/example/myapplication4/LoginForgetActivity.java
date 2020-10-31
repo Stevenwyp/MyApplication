bc7d18a209f3cdd59b8e067792f2201c3dcd3184
Stevenwyp <1608416210@qq.com>
2020-10-31 23:41:24 +0800

    first commit

--- MyApplication4/app/src/main/java/com/example/myapplication4/LoginForgetActivity.java
+++ MyApplication4/app/src/main/java/com/example/myapplication4/LoginForgetActivity.java
@@ -9,31 +9,51 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.myapplication4.bean.UserInfo;
+import com.example.myapplication4.database.UserDBHelper;
+import com.example.myapplication4.util.DateUtil;
+
 
 public class LoginForgetActivity extends AppCompatActivity implements View.OnClickListener{
-
     private EditText et_password_first; // 声明一个编辑框对象
     private EditText et_password_second; // 声明一个编辑框对象
     private EditText et_verifycode; // 声明一个编辑框对象
     private String mVerifyCode; // 验证码
     private String mPhone; // 手机号码
-    // private UserDBHelper mHelper; // 声明一个用户数据库的帮助器对象
+    private UserDBHelper mHelper; // 声明一个用户数据库的帮助器对象
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_forget);
-
-
         // 从布局文件中获取名叫et_password_first的编辑框
         et_password_first = findViewById(R.id.et_password_first);
         // 从布局文件中获取名叫et_password_second的编辑框
         et_password_second = findViewById(R.id.et_password_second);
         // 从布局文件中获取名叫et_verifycode的编辑框
         et_verifycode = findViewById(R.id.et_verifycode);
-
-
+        findViewById(R.id.btn_verifycode).setOnClickListener(this);
+        findViewById(R.id.btn_confirm).setOnClickListener(this);
+        // 从前一个页面获取要修改密码的手机号码
+        mPhone = getIntent().getStringExtra("phone");
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // 获得用户数据库帮助器的一个实例
+        mHelper = UserDBHelper.getInstance(this, 2);
+        // 恢复页面，则打开数据库连接
+        mHelper.openWriteLink();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // 暂停页面，则关闭数据库连接
+        mHelper.closeLink();
+    }
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.btn_verifycode) { // 点击了“获取验证码”按钮
@@ -53,7 +73,7 @@ public class LoginForgetActivity extends AppCompatActivity implements View.OnCli
         } else if (v.getId() == R.id.btn_confirm) { // 点击了“确定”按钮
             String password_first = et_password_first.getText().toString();
             String password_second = et_password_second.getText().toString();
-            if (password_first.length() < 6 || password_second.length() < 6) {
+            if (password_first.length() < 8 || password_second.length() < 8) {
                 Toast.makeText(this, "请输入正确的新密码", Toast.LENGTH_SHORT).show();
                 return;
             }
@@ -63,7 +83,20 @@ public class LoginForgetActivity extends AppCompatActivity implements View.OnCli
             }
             if (!et_verifycode.getText().toString().equals(mVerifyCode)) {
                 Toast.makeText(this, "请输入正确的验证码", Toast.LENGTH_SHORT).show();
+            } else {
+                UserInfo info = new UserInfo();
+                info.phone = mPhone;
+                info.pwd = password_first;
+                info.update_time = DateUtil.getNowDateTime("yyyy-MM-dd HH:mm:ss");
+                mHelper.update(info, "phone=" + mPhone);
+                Toast.makeText(this, "密码修改成功", Toast.LENGTH_SHORT).show();
+                // 把修改好的新密码返回给前一个页面
+                Intent intent = new Intent();
+                intent.putExtra("new_password", password_first);
+                setResult(Activity.RESULT_OK, intent);
+                finish();
             }
         }
     }
+
 }
